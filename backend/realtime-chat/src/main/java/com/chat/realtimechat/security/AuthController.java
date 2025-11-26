1b8430d92f1b93eef6e3f43725039651266fc264
mateusz <matikugu@gmail.com>
2025-11-26 22:56:02 +0100

    api returns a json

--- backend/realtime-chat/src/main/java/com/chat/realtimechat/security/AuthController.java
+++ backend/realtime-chat/src/main/java/com/chat/realtimechat/security/AuthController.java
@@ -4,14 +4,19 @@ package com.chat.realtimechat.security;
 import com.chat.realtimechat.domain.User;
 import com.chat.realtimechat.model.dto.request.LoginRequset;
 import com.chat.realtimechat.model.dto.request.RegistrationRequest;
+import com.chat.realtimechat.model.dto.response.LoginResponse;
 import com.chat.realtimechat.repository.UserRepository;
 import com.chat.realtimechat.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.jackson.JacksonComponentModule;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,15 +29,16 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody @Valid RegistrationRequest req) {
+    public ResponseEntity<User> register(@RequestBody @Valid RegistrationRequest req) {
         User registeredUser = userService.registerUser(req);
-        return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser.toString());
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody @Valid LoginRequset req) {
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequset req) {
         User loginUser = userService.authenticate(req.getUsername(), req.getPassword());
         String token = jwtUtil.generateToken(loginUser.getUsername());
-        return ResponseEntity.ok(token);
+        LoginResponse response = new LoginResponse(token);
+        return ResponseEntity.ok(response);
     }
 }
