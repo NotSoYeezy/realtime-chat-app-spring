325d2b1e79e3749bfadb31c346669ebe92b80f11
mateusz <matikugu@gmail.com>
2025-11-28 15:17:47 +0100

    extracting tokens by id

--- backend/realtime-chat/src/main/java/com/chat/realtimechat/service/security/CustomUserDetailsService.java
+++ backend/realtime-chat/src/main/java/com/chat/realtimechat/service/security/CustomUserDetailsService.java
@@ -1,27 +1,9 @@
 package com.chat.realtimechat.service.security;
 
-import com.chat.realtimechat.domain.User;
-import com.chat.realtimechat.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
-
-    private final UserRepository repo;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User u = repo.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
-
-        return org.springframework.security.core.userdetails.User.withUsername(u.getUsername())
-                .password(u.getPassword())
-                .roles("USER")
-                .build();
-    }
+public interface CustomUserDetailsService extends UserDetailsService {
+    UserDetails loadUserByUsername(String username);
+    UserDetails loadUserById(Long id);
 }
